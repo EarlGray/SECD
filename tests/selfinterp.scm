66e988cec7baa93fe7a0bb3978e59928ec3ffedc
EarlGray <dmytrish@gmail.com>
2014-02-18 02:43:23 +0200

    selfinterp.scm: working DUM/RAP

--- tests/selfinterp.scm
+++ tests/selfinterp.scm
@@ -1,26 +1,64 @@
+(define (caar lst) (car (car lst)))
 (define (cadr lst) (car (cdr lst)))
 (define (cddr lst) (cdr (cdr lst)))
 (define (caddr lst) (car (cddr lst)))
 (define (cdddr lst) (cdr (cddr lst)))
 
+(define (self-print-env env)
+  (letrec ((print-frame
+    (lambda (syms vals)
+      (if (null? syms)
+        (if (null? vals) '()
+          (begin (display "\n;; Error: trailing vals\n") (display vals) (newline)))
+        (if (null? vals)
+          (begin
+            (display "\n;; Error: no vals\n") (display syms) '())
+          (begin
+            (display "\n;;  ") (display (car syms)) (display "  -> ") (display (car vals))
+            (print-frame (cdr syms) (cdr vals))))))))
+    (if (null? env) '()
+      (let ((frame (car env)))
+        (if (null? frame)
+          (begin
+            (display "\n;; Error: null frame\n") '())
+          (begin
+            (display "\n;; Frame\n")
+            (if (vector? frame)
+                (begin
+                  (display "\n;;   ###")
+                  (let ((f (vector-ref frame 0)))
+                    (print-frame (car f) (cdr f))))
+                (print-frame (car frame) (cdr frame)))
+            (self-print-env (cdr env))))))))
+
 (define (self-lookup-env env sym)
-  (if (null? env) '()
-    (let ((frame (car env))
+  (if (null? env)
+    '()
+    (let ((frame (let ((f (car env)))
+                   (if (vector? f)  (vector-ref f 0) f)))
           (envrest (cdr env)))
-      (letrec ((lookup-frame 
-          (lambda (syms vals)
-            (if (null? syms) '()
-              (if (eq? (car syms) sym)
-                  (list (car vals))
-                  (lookup-frame (cdr syms) (cdr vals)))))))
-        (let ((res (lookup-frame (car frame) (cdr frame))))
-          (if (null? res)
-              (self-lookup-env envrest sym)
-              res))))))
+      (if (pair? frame)
+        (letrec ((lookup-frame
+            (lambda (syms vals)
+              (if (null? syms) '()
+                (if (eq? (car syms) sym)
+                    (list (car vals))
+                    (lookup-frame (cdr syms) (cdr vals)))))))
+          (if (eq? (length (car frame)) (length (cdr frame)))
+            (let ((res (lookup-frame (car frame) (cdr frame))))
+              (if (null? res)
+                  (self-lookup-env envrest sym)
+                  res))
+            (begin
+                (display "\nError:_args") (newline)
+                (self-print-env env)
+                '())))
+        ;; it's an omega-frame, skipping:
+        (self-lookup-env envrest sym)))))
 
 (define (self-eval-step s e c d)
   (let ((cmd (car c)) (c1 (cdr c)))
-    (begin 
+    (begin
       ;(display "\n\n  s = ") (display s)
       ;(display "\n  e = ") (display e)
       ;(display "\n  c = ") (display c)
@@ -32,9 +70,12 @@
         ((eq? cmd 'ADD)
           (let ((v1 (car s)) (v2 (cadr s)) (s2 (cddr s)))
             (self-eval-step (cons (+ v1 v2) s2) e c1 d)))
+        ((eq? cmd 'SUB)
+          (let ((v1 (car s)) (v2 (cadr s)) (s2 (cddr s)))
+            (self-eval-step (cons (- v1 v2) s2) e c1 d)))
         ((eq? cmd 'MUL)
           (let ((v1 (car s)) (v2 (cadr s)) (s2 (cddr s)))
-            (self-eval-step (cons (+ v1 v2) s2) e c1 d)))
+            (self-eval-step (cons (* v1 v2) s2) e c1 d)))
 
         ((eq? cmd 'CONS)
           (let ((v1 (car s)) (v2 (cadr s)) (s2 (cddr s)))
@@ -44,7 +85,9 @@
             (self-eval-step (cons (car v) s1) e c1 d)))
         ((eq? cmd 'CDR)
           (let ((v (car s)) (s1 (cdr s)))
-            (self-eval-step (cons (cdr v) s1) e c1 d)))
+            (if (null? v)
+              'Error:_cdr_nil
+              (self-eval-step (cons (cdr v) s1) e c1 d))))
 
         ((eq? cmd 'LEQ)
           (let ((v1 (car s)) (v2 (cadr s)) (s2 (cddr s)))
@@ -62,12 +105,13 @@
             (self-eval-step s1 e (if v thenb elseb) (cons c2 d))))
         ((eq? cmd 'JOIN)
           (let ((c1 (car d)) (d1 (cdr d)))
-            (self-eval-step s e c1 d)))
+            (self-eval-step s e c1 d1)))
 
         ((eq? cmd 'LD)
           (let ((cn (car c1)) (c2 (cdr c1)))
             (let ((v (self-lookup-env e cn)))
-              (if (null? v) (list 'Error:_lookup_failed_for cn)
+              (if (null? v)
+                (list 'Error:_lookup_failed_for cn)
                 (self-eval-step (cons (car v) s) e c2 d)))))
         ((eq? cmd 'LDF)
           (let ((func (car c1)) (c2 (cdr c1)))
@@ -77,7 +121,12 @@
           (let ((clos (car s)) (argvals (cadr s)) (s2 (cddr s)))
             (let ((func (car clos)) (e1 (cdr clos)))
               (let ((args (car func)) (body (cadr func)))
-                (self-eval-step '() (cons (cons args argvals) e1) body (append (list s2 e c1) d))))))
+                (let ((d1 (append (list s2 e c1) d)))
+                  (begin
+                    ;(display "\n; argnames: ") (display args)
+                    ;(display "\n; argvals : ") (display argvals) (newline)
+                    ;(self-print-env e1)
+                    (self-eval-step '() (cons (cons args argvals) e1) body d1)))))))
         ((eq? cmd 'RTN)
           (let ((v (car s))
                 (s1 (car d)) (e1 (cadr d)) (c1 (caddr d)) (d1 (cdddr d)))
@@ -90,7 +139,20 @@
           (begin
             (display (car s))
             (self-eval-step s e c d)))
-        ;; TODO: DUM, RAP
+
+        ((eq? cmd 'DUM)
+            (self-eval-step s (cons (make-vector 1 0) e) c1 d))
+        ((eq? cmd 'RAP)
+          (let ((clos (car s)) (argvals (cadr s)) (s2 (cddr s)))
+            (let ((func (car clos)) (e1 (cdr clos)))
+              (let ((args (car func)) (body (cadr func)))
+                (let ((frame (cons args argvals))
+                      (d1 (append (list s2 (cdr e1) c1) d)))
+                  (begin
+                    ;(display "\n; RAP argnames: ") (display args)
+                    ;(display "\n; RAP  argvals: ") (display argvals)
+                    (vector-set! (car e1) 0 frame)
+                    (self-eval-step '() e1 body d1)))))))
 
         ((eq? cmd 'STOP)
           (car s))
